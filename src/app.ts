92648caf82abd5a8515065c1678da098448a7045
HawkEyeVe <deutsch.escalante@gmail.com>
2025-02-19 11:07:37 -0400

    game board and movement

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,29 @@
 class DivCreator {
-  private div: HTMLElement;
+  private _div: HTMLElement;
   constructor(id: string, divType: string = "div") {
-    this.div = document.createElement(divType);
-    this.div.id = id;
+    this._div = document.createElement(divType);
+    this._div.id = id;
   }
 
   public appendTo(element: HTMLElement) {
-    element.appendChild(this.div);
+    element.appendChild(this._div);
   }
 
-  public get Div() {
-    return this.div;
+  public get div() {
+    return this._div;
   }
 
   public applyStyles(styles: Record<string, string>): void {
     for (const [property, value] of Object.entries(styles)) {
       if (value !== undefined) {
-        this.div.style.setProperty(property, value);
+        this._div.style.setProperty(property, value);
       }
     }
   }
 }
 
 class Player {
+  player: DivCreator;
   appendTo: HTMLElement;
   position: {
     x: number;
@@ -35,51 +36,70 @@ class Player {
       x: 0,
       y: 0,
     };
-  }
-
-  render(): void {
-    const player = new DivCreator("player", "div");
-    player.appendTo(this.appendTo);
-    player.applyStyles({
+    this.player = new DivCreator("player", "div");
+    this.player.appendTo(this.appendTo);
+    this.player.applyStyles({
       width: "50px",
       height: "50px",
       background: "red",
-      position: "relative",
+      position: "absolute",
       top: `${this.position.y}px`,
       left: `${this.position.x}px`,
     });
   }
 
-  move(): void {
+  moveEvent(): void {
     window.addEventListener("keydown", (event) => {
       switch (event.key) {
         case "ArrowUp":
-          this.position.y -= 2;
-          this.render();
+          this.position.y -= 10;
+          this.player.applyStyles({
+            top: `${this.position.y}px`,
+          });
           break;
         case "ArrowDown":
-          this.position.y += 2;
-          this.render();
+          this.position.y += 10;
+          this.player.applyStyles({
+            top: `${this.position.y}px`,
+          });
           break;
         case "ArrowLeft":
-          this.position.x -= 2;
-          this.render();
+          this.position.x -= 10;
+          this.player.applyStyles({
+            left: `${this.position.x}px`,
+          });
           break;
         case "ArrowRight":
-          this.position.x += 2;
-          this.render();
+          this.position.x += 10;
+          this.player.applyStyles({
+            left: `${this.position.x}px`,
+          });
           break;
       }
     });
   }
 }
-const playableArea = new DivCreator("box", "div");
-playableArea.appendTo(document.body);
-playableArea.applyStyles({
-  width: "400px",
-  height: "400px",
-  background: "green",
-});
 
-const player = new Player(playableArea.Div);
-player.move();
+class GameBoard {
+  playableArea: DivCreator;
+  player: Player;
+  constructor() {
+    this.playableArea = new DivCreator("box", "div");
+    this.playableArea.appendTo(document.body);
+    this.playableArea.applyStyles({
+      width: "400px",
+      height: "400px",
+      background: "green",
+      position: "relative",
+      border: "20px solid black",
+    });
+    this.player = new Player(this.playableArea.div);
+  }
+}
+
+function main() {
+  const game = new GameBoard();
+  game.player.moveEvent();
+}
+
+main();
