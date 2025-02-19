b9e179f64b062e62626b57d12f86959c6397262d
HawkEyeVe <deutsch.escalante@gmail.com>
2025-02-19 14:45:19 -0400

    collision

--- src/app.ts
+++ src/app.ts
@@ -48,29 +48,33 @@ class Player {
     });
   }
 
-  moveEvent(): void {
+  moveEvent(collision: GameBoard): void {
     window.addEventListener("keydown", (event) => {
       switch (event.key) {
         case "ArrowUp":
           this.position.y -= 10;
+          collision.borderColision();
           this.player.applyStyles({
             top: `${this.position.y}px`,
           });
           break;
         case "ArrowDown":
           this.position.y += 10;
+          collision.borderColision();
           this.player.applyStyles({
             top: `${this.position.y}px`,
           });
           break;
         case "ArrowLeft":
           this.position.x -= 10;
+          collision.borderColision();
           this.player.applyStyles({
             left: `${this.position.x}px`,
           });
           break;
         case "ArrowRight":
           this.position.x += 10;
+          collision.borderColision();
           this.player.applyStyles({
             left: `${this.position.x}px`,
           });
@@ -90,16 +94,52 @@ class GameBoard {
       width: "400px",
       height: "400px",
       background: "green",
-      position: "relative",
+      position: "absolute",
       border: "20px solid black",
     });
     this.player = new Player(this.playableArea.div);
+  }
+
+  borderColision() {
+    if (
+      this.player.position.x + 50 >=
+      parseInt(this.playableArea.div.style.width)
+    ) {
+      this.player.position.x = 350;
+      this.player.player.applyStyles({
+        background: "blue",
+      });
+    }
+
+    if (this.player.position.x < 0) {
+      this.player.position.x = 0;
+      this.player.player.applyStyles({
+        background: "purple",
+      });
+    }
+
+    if (
+      this.player.position.y + 50 >=
+      parseInt(this.playableArea.div.style.height)
+    ) {
+      this.player.position.y = 350;
+      this.player.player.applyStyles({
+        background: "yellow",
+      });
+    }
+
+    if (this.player.position.y < 0) {
+      this.player.position.y = 0;
+      this.player.player.applyStyles({
+        background: "orange",
+      });
+    }
   }
 }
 
 function main() {
   const game = new GameBoard();
-  game.player.moveEvent();
+  game.player.moveEvent(game);
 }
 
 main();
